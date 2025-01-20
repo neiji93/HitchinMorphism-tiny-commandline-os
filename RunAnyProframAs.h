758cfaf6f8dcfc8c994dc929bba6bf0e61eb072c
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-20 13:31:19 +0100

    Create RunAnyProframAs.h

---
+++ RunAnyProframAs.h
@@ -0,0 +1,5 @@
+//i have to wait for tom gracey formulas
+//to enter those logics. he is working with me with the dirac os project.
+//as you pleased, you must notice there is z link with another comma'd
+//line tool. but the single formula that he would gave to me
+//will help us to..... im tired.
