1f0a8deb62c060fa87dc033ad72fb0871f88df67
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-21 05:26:04 +0100

    Update SurjectiveSignalPropagator.h

--- SurjectiveSignalPropagator.h
+++ SurjectiveSignalPropagator.h
@@ -1,1 +1,1 @@
-
+//https://www.google.fr/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.math.uchicago.edu/~ngo/Chen-Ngo.pdf&ved=2ahUKEwjBpqyF-oWLAxWsTKQEHdMwJpsQFnoECBMQAQ&usg=AOvVaw1PsK9XE_clkunaJuD25LLM
