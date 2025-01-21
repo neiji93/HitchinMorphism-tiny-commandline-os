600ebf43bd02d592013a6467f3fcf5638533fdf8
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-21 05:53:09 +0100

    Update MainMeanFieldGameEnhancedMathSupFR.h

--- MainMeanFieldGameEnhancedMathSupFR.h
+++ MainMeanFieldGameEnhancedMathSupFR.h
@@ -1,3 +1,9 @@
 //need math sup lectures
 //and
 //see: https://www.google.fr/books/edition/Mean_Field_Games/Iy8WEAAAQBAJ?hl=fr&gbpv=1&dq=latest+mean+field+games&printsec=frontcover
+
+
+//see: https://maths-france.fr/wp-content/uploads/16_primitives_planche.pdf
+//Let's resolve some integrals. Here, it emans that we roughly have distributions.
+//So let's keep track ....humm
+//In order to forbid using scientific pocess compilers, we better have to have a scheduller like in video games
